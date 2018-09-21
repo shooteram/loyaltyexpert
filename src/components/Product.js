d4860f94d349b118d007ddf3d6179cd5c1d4021d
shooteram <me@shooteram.fr>
2018-09-21 18:25:13 +0200

    update the Product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,8 +1,37 @@
 import React from "react";
+import Category from "./Category";
+import Brand from "./Brand";
 
 class Product extends React.Component {
   render() {
-    return <div />;
+    const { name, image, description, categories, brand } = {
+      ...this.props.data,
+    };
+
+    return (
+      <div className="text-black no-underline flex w-full lg:w-1/2 px-2 mb-3">
+        <div className="border-b-2 shadow hover:shadow-md transition w-full bg-white rounded-lg p-4 flex leading-normal">
+          <div className="w-1/4 text-center">
+            {image && <img className="rounded-full w-16 h-16" src={image} />}
+
+            {categories.length > 0 &&
+              categories.map((category, key) => {
+                return <Category key={key} data={category} />;
+              })}
+          </div>
+
+          <div className="w-3/4">
+            {brand && <Brand data={brand} />}
+
+            <h4 className="truncate mb-2" title={name}>
+              {name}
+            </h4>
+
+            <p>{description}</p>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
