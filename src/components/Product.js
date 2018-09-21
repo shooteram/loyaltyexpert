9dab0d1bdce1337874b18c6a9e3bf38a6b371aab
shooteram <me@shooteram.fr>
2018-09-21 21:10:14 +0200

    add empty alt prop for products images
    to avoid React's warning in the console.

--- src/components/Product.js
+++ src/components/Product.js
@@ -12,7 +12,9 @@ class Product extends React.Component {
       <div className="text-black no-underline flex w-full lg:w-1/2 px-2 mb-3">
         <div className="border-b-2 shadow hover:shadow-md transition w-full bg-white rounded-lg p-4 flex leading-normal">
           <div className="w-1/4 text-center">
-            {image && <img className="rounded-full w-16 h-16" src={image} />}
+            {image && (
+              <img className="rounded-full w-16 h-16" src={image} alt="" />
+            )}
 
             {categories.length > 0 &&
               categories.map((category, key) => {
