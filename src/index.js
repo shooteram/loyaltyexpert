d7e14d0762bf3c7ef42771808cd9b5f995699ac7
shooteram <me@shooteram.fr>
2018-09-21 17:21:00 +0200

    add basic configuration for axios
    with the api as its baseURL.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://test-recrutement.loyaltyexpert.net/";
+axios.defaults.headers.common = {
+  accept: "application/json",
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
