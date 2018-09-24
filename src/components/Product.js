6948893ca07e35139d0f305119dc017b80e007e3
shooteram <me@shooteram.fr>
2018-09-24 18:30:39 +0200

    update Product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,26 +3,46 @@ import Category from "./Category";
 import Brand from "./Brand";
 
 class Product extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { imageLoaded: false };
+    this.imageLoaded = this.imageLoaded.bind(this);
+  }
+
+  imageLoaded() {
+    this.setState({ imageLoaded: true });
+  }
+
   render() {
     const { name, image, description, categories, brand } = {
       ...this.props.data,
     };
+    const { onClick } = { ...this.props };
+    const { imageLoaded } = { ...this.state };
 
     return (
-      <div className="text-black no-underline flex w-full lg:w-1/2 px-2 mb-3">
-        <div className="border-b-2 shadow hover:shadow-md transition w-full bg-white rounded-lg p-4 flex leading-normal">
-          <div className="w-1/4 text-center">
+      <div
+        onClick={onClick}
+        className="text-black no-underline flex w-full lg:w-1/2 px-2 mb-3"
+      >
+        <div className="border-b-2 shadow hover:shadow-md w-full bg-white rounded-lg p-4 flex leading-normal cursor-pointer">
+          <div
+            className={`w-1/4 mr-4 text-center ${
+              imageLoaded ? "visible" : "invisible absolute"
+            }`}
+          >
             {image && (
-              <img className="rounded-full w-16 h-16" src={image} alt="" />
+              <img
+                onLoad={this.imageLoaded}
+                className="rounded-full w-16 h-16"
+                src={image}
+                alt=""
+              />
             )}
-
-            {categories.length > 0 &&
-              categories.map((category, key) => {
-                return <Category key={key} data={category} />;
-              })}
           </div>
 
-          <div className="w-3/4">
+          <div className={`transition ${imageLoaded ? "w-3/4" : "w-full"}`}>
             {brand && <Brand data={brand} />}
 
             <h4 className="truncate mb-2" title={name}>
@@ -30,6 +50,13 @@ class Product extends React.Component {
             </h4>
 
             <p>{description}</p>
+
+            <div className="float-right">
+              {categories.length > 0 &&
+                categories.map((category, key) => {
+                  return <Category key={key} data={category} />;
+                })}
+            </div>
           </div>
         </div>
       </div>
