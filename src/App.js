27b7e4757f16e1edfc9013a196ca39e69a185300
shooteram <me@shooteram.fr>
2018-09-24 18:33:14 +0200

    update main component -- wip
    it's a wip.
    the "submit" buttons echoes form values inside the console for now.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,22 @@ import Product from "./components/Product";
 import SearchBar from "./components/SearchBar";
 import Pagination from "react-js-pagination";
 import { chunk } from "lodash";
+import Modal from "./components/Modal";
+import Error from "./components/Error";
+import { getBrandsAndCategoriesFromProducts } from "./helpers";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { products: null, visibleProducts: null, activePage: 1 };
+    this.state = {
+      activePage: 1,
+      perPage: 6,
+    };
+
+    this.handleFormChanges = this.handleFormChanges.bind(this);
     this.handlePageChange = this.handlePageChange.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
+    this.dismissForm = this.dismissForm.bind(this);
   }
 
   componentDidMount() {
@@ -19,63 +28,150 @@ class App extends React.Component {
   }
 
   handlePageChange(page) {
-    let products = chunk(this.state.products, 10);
+    let products = chunk(this.state.products, this.state.perPage);
     this.setState({ visibleProducts: products[page - 1], activePage: page });
   }
 
   componentDidUpdate() {
-    if (this.props.products && !this.state.visibleProducts) {
-      this.setState({ products: this.props.products });
+    const products = this.props.products;
+
+    if (products && !this.state.visibleProducts) {
+      const { brands, categories } = getBrandsAndCategoriesFromProducts(
+        products,
+      );
+
+      this.setState({
+        products: products,
+        categories: categories,
+        brands: brands,
+      });
+
       this.handlePageChange(this.state.activePage);
     }
   }
 
   handleSearch(e) {
     let value = e.target.value;
-    if (!value) return this.handlePageChange(1);
+
+    if (!value) {
+      this.setState({ searching: false });
+      return this.handlePageChange(1);
+    }
 
     let result = this.state.products.filter(
       product => product.name.indexOf(value) >= 0,
     );
 
-    this.setState({ visibleProducts: result });
+    this.setState({ visibleProducts: result, searching: true });
+  }
+
+  handleClickOnProduct(product) {
+    this.setState({ product: product || {}, form: product || {} });
+  }
+
+  handleFormSubmit(e, data) {
+    e.preventDefault();
+    console.log(data);
+  }
+
+  handleFormChanges(e) {
+    let form = this.state.form;
+    let product = this.state.product;
+    let update = { [e.target.name]: e.target.value };
+
+    if (!form.id && product && product.id) update.id = product.id;
+
+    this.setState({
+      form: Object.assign({}, form, update),
+    });
+  }
+
+  dismissForm() {
+    this.setState({ form: {}, product: null });
   }
 
   render() {
     const { fetching, error } = { ...this.props };
-    const { products, visibleProducts } = { ...this.state };
+    const {
+      visibleProducts,
+      categories,
+      searching,
+      products,
+      product,
+      brands,
+      form,
+    } = {
+      ...this.state,
+    };
 
     return fetching ? (
       <div>fetching ...</div>
     ) : (
       <div>
-        <div className="container mx-auto p-4">
+        <div className="container mx-auto p-4" style={{ marginBottom: "40px" }}>
           {visibleProducts && (
             <React.Fragment>
               <h2 className="mb-4">Products ({products.length})</h2>
-              <SearchBar onChange={this.handleSearch} className="mb-3 w-full" />
 
-              <div className="flex flex-wrap -mx-2 cursor-default">
-                {visibleProducts.map((product, key) => {
-                  return <Product key={key} data={product} />;
+              <div className="md:flex">
+                <button
+                  onClick={_ => this.handleClickOnProduct(null)}
+                  className="border border-green hover:bg-green-light text-green hover:text-white font-bold py-2 px-8 rounded mb-3 md:mr-2 w-full md:w-1/3"
+                >
+                  Create a new product
+                </button>
+
+                <SearchBar
+                  onChange={this.handleSearch}
+                  className="mb-3 w-full"
+                />
+              </div>
+
+              {product && (
+                <Modal
+                  submit={e => this.handleFormSubmit(e, form)}
+                  update={this.handleFormChanges}
+                  allCategories={categories}
+                  close={this.dismissForm}
+                  brands={brands}
+                  data={product}
+                />
+              )}
+
+              <div className="flex flex-wrap -mx-2">
+                {visibleProducts.map(product => {
+                  return (
+                    <Product
+                      onClick={_ => this.handleClickOnProduct(product)}
+                      key={product.id}
+                      data={product}
+                    />
+                  );
                 })}
               </div>
 
-              <div className="text-center">
-                <Pagination
-                  hideDisabled
-                  activePage={this.state.activePage}
-                  itemsCountPerPage={10}
-                  totalItemsCount={products.length}
-                  pageRangeDisplayed={5}
-                  onChange={this.handlePageChange}
-                  activeLinkClass="active"
-                />
-              </div>
+              {!searching && (
+                <div
+                  className="z-50 text-center fixed pin-b pin-l pin-r"
+                  style={{ zIndex: "auto" }}
+                >
+                  <Pagination
+                    itemsCountPerPage={this.state.perPage}
+                    activePage={this.state.activePage}
+                    totalItemsCount={products.length}
+                    onChange={this.handlePageChange}
+                    activeLinkClass="active"
+                    pageRangeDisplayed={4}
+                    hideDisabled
+                  />
+                </div>
+              )}
             </React.Fragment>
           )}
 
-          <p>{error}</p>
+          {error && (
+            <Error reload={_ => window.location.reload()} description={error} />
+          )}
         </div>
       </div>
     );
